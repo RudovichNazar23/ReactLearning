548f07f191839d0a262cbcb7948da73ed55751ae
RudovichNazar23 <n.rudovich2005@gmail.com>
2023-12-25 19:47:01 +0100

    created first react app usign CRA and created new component using jsx

---
+++ first-react-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/HelloWorldJSX';
+
+function App() {
+  return (
+      <HelloWorld></HelloWorld>
+  );
+}
+
+export default App;

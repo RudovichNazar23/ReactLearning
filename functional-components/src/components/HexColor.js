6d6549133a6c4ea38ec7e56a8f07eac3e91a6da5
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-02-05 15:39:55 +0100

    added two more components where I filtered input values and was optimizing form handlers

---
+++ functional-components/src/components/HexColor.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const PLACEHOLDER = `conic-gradient(
+    gray 0.25turn white 0 0.5turn,
+    gray 0 0.75turn, white 0 1turn
+)`;
+
+function HexColor(){
+    const [color, setColor] = useState("BADA55");
+    const onChange = (event) => {
+        setColor(
+            event.target.value
+            .replace(/[^0-9a-f]/gi, "")
+            .toUpperCase()
+        );
+    }
+    const outpuStyle = {
+        width: "20px",
+        border: "1px solid",
+        background: color.length === 6 
+            ? `#${color}`
+            : PLACEHOLDER
+    }
+
+    return (
+        <form style={{display: "flex"}}>
+            <label>
+                Hex color:
+            </label>
+            <input type="text" value={color} onChange={onChange} />
+            <span style={outpuStyle} />
+        </form>
+    )
+}
+
+export default HexColor;
